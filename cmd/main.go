d5d6c8cf0db2e04daa2c665a393c66d16d4f330b
Alexander Bulychev <git@bulychev.pro>
2025-12-14 17:36:14 +0400

    Add html/template usage

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,16 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 	slogfiber "github.com/samber/slog-fiber"
 )
 
 func main() {
 	config.Init()
-	app := fiber.New()
+	engine := html.New("./html", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	app.Use(slogfiber.New(logger))
--- go.mod
+++ go.mod
@@ -8,7 +8,13 @@ require (
 )
 
 require (
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/gofiber/template/html/v2 v2.1.3
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
